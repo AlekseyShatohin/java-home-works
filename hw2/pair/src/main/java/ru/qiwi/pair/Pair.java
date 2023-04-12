bf8a0064b13a121a806a368d3b8600aa5f64dc91
AlekseyShatohin <al.shatokhin@qiwi.com>
2023-04-12 11:56:09 +0300

    feat(homework 2): pair

--- hw2/pair/src/main/java/ru/qiwi/pair/Pair.java
+++ hw2/pair/src/main/java/ru/qiwi/pair/Pair.java
@@ -2,4 +2,35 @@ package ru.qiwi.pair;
 
 public class Pair<One, Two> {
 
+    private final One first;
+
+    private final Two second;
+
+    private Pair(One first, Two second) {
+        this.first = first;
+        this.second = second;
+    }
+
+
+    public static <One, Two> Pair<One, Two> of(One first, Two second) {
+        return new Pair<>(first, second);
+
+    }
+
+    public One getFirst() {
+        return first;
+    }
+
+    public Two getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getClass() == obj.getClass()) {
+            return first == ((Pair<?, ?>) obj).getFirst() && second == ((Pair<?, ?>) obj).getSecond();
+        } else {
+            return false;
+        }
+    }
 }
